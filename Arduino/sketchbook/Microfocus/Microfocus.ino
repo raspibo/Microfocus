00ba0349bffe129b0b14a6d7722f0226adf6b3e3
dcast78 <dcast78@gmail.com>
2019-12-12 12:02:34 +0100

    Add support for infrared tv remote control.
    Fisrt use is for microscope connected to pc, but my microscope is compatible with Android app.
    Sending command from pc is simple, but with Android is required to move the servo with another system.
    A simple system is to connect ir receiver and use irremote lib.

--- Arduino/sketchbook/Microfocus/Microfocus.ino
+++ Arduino/sketchbook/Microfocus/Microfocus.ino
@@ -1,52 +1,78 @@
-/*
-  Controlling a servo position using a potentiometer (variable resistor)
-  by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
 
-  modified on 8 Nov 2013
-  by Scott Fitzgerald
-  http://www.arduino.cc/en/Tutorial/Knob
-*/
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
 
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val = 90;  // variable to read the value from the analog pin
-long last_mod = 0;
+Servo myservo;      // create servo object to control a servo
+int inp = 0;        // value read from serial port
+int ir = 0;         // value read from infrared
+int last_ir = 0;    // last value read from infrared (some ir commands after first button press repeat a sequence of 0xFFFFFFFF, last_ir contain last first pressed button)
+int val = 90;       // variable to read the value from the analog pin
+int last_val = 0;   // variable to store last val (last position of servo) used to reduce serial prints
+long last_mod = 0;  // store last pos change time to activate power saving
+#define POWERSAVE_MILLIS 3000
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the infrared receiver
 }
 
 void loop() {
+  if (irrecv.decode(&results)) {
+    //Serial.println(results.value, HEX); // Debug for setup
+    irrecv.resume(); // Receive the next value
+    if (results.value == 0x1FE08F7) {     // Up button on direction cross keys
+      ir = '+';
+      last_ir = ir;
+    }
+    if (results.value == 0x1FE10EF) {     // Down button on direction cross keys
+      ir = '-';
+      last_ir = ir;
+    }
+    if (results.value == 0xFFFFFFFF) {    // Code of last key repeat
+      ir = last_ir;
+    }
+  }
   while (Serial.available() > 0) {
-    int inp = Serial.read();
-    if (inp == '+') {
-      if (val <= 255) {
-        val++;
-        last_mod = millis();
-      }
+    inp = Serial.read();
+  }
+  if (inp == '+' or ir == '+') {
+    if (val <= 255) {
+      val++;
+      last_mod = millis();
     }
-    if (inp == '-') {
-      if (val >= 1) {
-        val--;
-        last_mod = millis();
-      }
+  }
+  if (inp == '-' or ir == '-') {
+    if (val >= 1) {
+      val--;
+      last_mod = millis();
     }
-    if (val > 0) {
-      if (val < 255) {
-        myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-        myservo.write(val);                  // sets the servo position according to the scaled value
+  }
+  if (val > 0) {
+    if (val < 255) {
+      myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+      myservo.write(val);                  // sets the servo position according to the scaled value
+      if (val != last_val) {
         Serial.print("valore: ");
         Serial.println(val);
+        last_val = val;
       }
     }
-    delay(15);                           // waits for the servo to get there
   }
-  if (millis() > last_mod + 3000) {
+  delay(15);                           // waits for the servo to get there
+  ir = 0;
+  inp = 0;
+
+  if (millis() > last_mod + POWERSAVE_MILLIS) {
     myservo.detach();
   }
 }
-
